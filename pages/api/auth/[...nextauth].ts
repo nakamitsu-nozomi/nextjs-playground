045415dd499abbc84c26eba056199950226af91f
nakamitsu_nozomi <exnnss0131@gmail.com>
2022-10-17 20:22:21 +0900

    feat: add secret

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,6 +6,9 @@ if (typeof process.env.GITHUB_OAUTH_CLIENT_ID !== 'string') {
 if (typeof process.env.GITHUB_OAUTH_CLIENT_SECRET !== 'string') {
   throw new Error('Undefined GITHUB_OAUTH_CLIENT_SECRET')
 }
+if (typeof process.env.NEXTAUTH_SECRET !== 'string') {
+  throw new Error('Undefined GITHUB_OAUTH_CLIENT_SECRET')
+}
 export default NextAuth({
   providers: [
     GitHubProvider({
@@ -13,6 +16,7 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_OAUTH_CLIENT_SECRET,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   callbacks: {
     async jwt({ token, account }) {
       if (account?.accessToken) {
